fb95e92c31e890f570d1b9e87c4f7f7b0b4a576a
onkarcoder750 <150378301+onkarcoder750@users.noreply.github.com>
2024-10-24 16:26:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello world program...");
     System.out.println("Java is the most popular programming language...");
-    System.out.println("Added changes in the remote repository");
-    System.out.println("Added changes in the local repository");
+    System.out.println("Added changes 1 in the remote repository");
+    System.out.println("Added changes 1 in the local repository");
+    System.out.println("Added changes 2 in the remote repository");
   }
 }
