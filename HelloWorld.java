a1abe1bad51f80ed063eace9f4927aab4ee63e46
onkarcoder750 <onkark750@gmail.com>
2024-10-24 16:00:09 +0530

    Add HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("Hello world program...");
+    System.out.println("Java is the most popular programming language...");
+    System.out.println("Added changes in the remote repository");
+  }
+}
