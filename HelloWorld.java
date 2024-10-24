50ac0cd1c9ef096018c23242c028075eaea07132
onkarcoder750 <onkark750@gmail.com>
2024-10-24 16:24:41 +0530

    Add the changes in the HelloWorld.java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
     System.out.println("Hello world program...");
     System.out.println("Java is the most popular programming language...");
     System.out.println("Added changes in the remote repository");
+    System.out.println("Added changes in the local repository");
   }
 }
